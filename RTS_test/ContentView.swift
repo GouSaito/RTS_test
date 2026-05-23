449201efc73c2c806bb96370c58b7b4731c0039b
GouSaito <tys1029@gmail.com>
2026-05-23 13:37:47 +0900

    Initial Commit

---
+++ RTS_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RTS_test
+//
+//  Created by 斎藤剛 on 2026/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RTS_test/RTS_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  RTS_testApp.swift
+//  RTS_test
+//
+//  Created by 斎藤剛 on 2026/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RTS_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
